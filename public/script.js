b187ea850d7c364d80eac860e0a40806d6e70d4b
Michael <mradzwilla@gmail.com>
2016-02-22 16:08:25 -0500

    Added candy falling from pinata

--- public/script.js
+++ public/script.js
@@ -7,4 +7,21 @@ $("#imageLink").click(function(){
 
 })
 
+y = 1
+$(".pinata").click(function(){
+	r = Math.floor(Math.random()*3)
+	candy1 = "images/candy1.png"
+	candy2 = "images/candy2.png"
+	candy3 = "images/candy3.png"
+	candyArr = [candy1, candy2, candy3]
+	x = $(".pinata").offset();
+	x.top += -550
+	x.left += 150
+	console.log(x)
+
+	console.log(x)
+	$(".candyland").append("<img src='" + candyArr[r] + "' id='candy" + y +"' class='candy'>");
+	$("#candy"+y).css({left: x.left, top: x.top}).animate({top:0}, 1500, 'linear');
+	y = y + 1;
+})
 })//Closes .ready
